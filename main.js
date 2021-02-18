4bbd21ebba7c76bfcbe4786138da71dfab5402e1
Timothy Negron <timothy.negron@codeimmersives.com>
2021-02-18 12:25:54 -0500

    Adds answers. All test passed.

--- main.js
+++ main.js
@@ -2,10 +2,37 @@
  * YOUR CODE HERE! *
  *******************/
 
+function yell (string) {
+  return string + '!';
+}
 
+function getFirstCharacter (string) {
+  return string[0];
+}
 
+function getLastCharacter (string) {
+  return string[string.length - 1];
+}
 
+function getOneCharacter (string, num) {
+  return string[num];
+}
 
+function getTwoCharacters (string, num1, num2) {
+  return string[num1] + string[num2];
+}
+
+function makeCapitalized (string) {
+  return string.toUpperCase();
+}
+
+function yellLouder (string) {
+  return string.toUpperCase() + '!!!';
+}
+
+function getInitials (name) {
+  return name[0] + '.' + name[name.indexOf(' ') + 1] + '.';
+}
 
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
